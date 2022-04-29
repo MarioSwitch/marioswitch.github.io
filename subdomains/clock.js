054d12240abf5aeb5a6f0f4469fec7049cfb55ed
MarioSwitch <58565980+MarioSwitch@users.noreply.github.com>
2022-04-29 23:23:40 +0200

    Delete subdomains directory

--- subdomains/clock.js
+++
@@ -1,6 +0,0 @@
-function clock(){
-    let date = new Date();
-    document.getElementById("UTCclock").innerHTML = ("0"+date.getUTCHours()).slice(-2)+":"+("0"+date.getUTCMinutes()).slice(-2)+":"+("0"+date.getUTCSeconds()).slice(-2);
-    document.getElementById("FRclock").innerHTML = ("0"+((date.getUTCHours()+2)%24)).slice(-2)+":"+("0"+date.getUTCMinutes()).slice(-2)+":"+("0"+date.getUTCSeconds()).slice(-2);
-    setTimeout(clock,1000);
-}
