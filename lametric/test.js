71c70baaa457504a441a12041ccbc378d5bf86a7
MarioSwitch <58565980+MarioSwitch@users.noreply.github.com>
2022-09-03 10:46:52 +0200

    Delete test.js

--- lametric/test.js
+++
@@ -1,8 +0,0 @@
-var url = 'http://www.marioswitch.fr/lametric/test.json';
-var request = new XMLHttpRequest();
-request.open('GET',url);
-request.send();
-request.onload = function() {
-    var resultat = request.response;
-    console.log(resultat);
-}
